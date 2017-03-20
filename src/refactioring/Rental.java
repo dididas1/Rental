c69ddb036860338414f7cc92a5956963da6e9741
lsw <dididas@naver.com>
2017-03-20 11:29:11 +0900

    커밋커밋

---
+++ src/refactioring/Rental.java
@@ -0,0 +1,22 @@
+package refactioring;
+public class Rental {
+	private int daysRented;
+	private Movie movie;
+	
+	public Rental(int daysRented, Movie movie) {
+		this.daysRented = daysRented;
+		this.movie = movie;
+	}
+	public int getDaysRented() {
+		return daysRented;
+	}
+	public void setDaysRented(int daysRented) {
+		this.daysRented = daysRented;
+	}
+	public Movie getMovie() {
+		return movie;
+	}
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+}
